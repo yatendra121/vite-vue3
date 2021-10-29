6aa48ee3abcab7731dc464621d5960bc71851959
yatendra <yatendra@singsys.com>
2021-10-29 17:21:41 +0530

    fixed icons

--- admin2/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/VitePackage/vite-vue3/admin/sw.js', { scope: '/VitePackage/vite-vue3/admin/' })})}
